fd80aa67c2b1a6cd9b6f4504e59020973a532ffb
hahoang1903 <duchahoang1903@gmail.com>
2021-05-17 22:43:43 +0700

    feat: add custom page loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import 'antd/dist/antd.css'
 import '../styles/main.scss'
 import { AuthProvider } from '../hooks/useAuth'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+
+Router.events.on('routeChangeStart', url => {
+	console.log(`Loading: ${url}`)
+	NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 export default function MyApp({ Component, pageProps }) {
 	return (
